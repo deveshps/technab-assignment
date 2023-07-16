577055ca1c890056cfe63302647df079aa8bff1b
deveshps <deveshps@dewsolutions.in>
2023-07-16 19:23:41 +0530

    Updates

--- src/components/notFoundpage.tsx
+++
@@ -1,9 +0,0 @@
-import { useNavigate } from "react-router";
-import styles from "../styles/pageNotFound.module.css";
-
-const NotFoundPage =() => {
-    const navigate = useNavigate();
-    return <div onClick={() => navigate("/")} className={styles.topWrapper}>NotFoundPage</div>
-}
-
-export default NotFoundPage;
